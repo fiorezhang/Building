c867f2d71085c1c34d173c3d090a63724231afa7
fiorezhang <fiorezhang@sina.com>
2018-08-19 23:23:54 +0800

    draw cloud, crane, ruler

--- demo_building.py
+++ demo_building.py
@@ -12,16 +12,44 @@ from pygame.locals import *
 #全局
 WINDOW_WIDTH = 800
 WINDOW_HEIGHT = 600
-FPS = 30
+FPS = 15
 MUSIC = True
 
 #参数设定
 BASIC_SPEED = WINDOW_WIDTH//400
 
-CLOUD_CNT = 6
 CLOUD_WIDTH = 100
 CLOUD_HEIGHT = 75
 CLOUD_SPEED = BASIC_SPEED
+CLOUD_CNT = 5
+
+RULER_WIDTH = 120
+RULER_HEIGHT = 300
+RULER_X = WINDOW_WIDTH-RULER_WIDTH
+RULER_Y = WINDOW_HEIGHT-RULER_HEIGHT
+
+LIFT_WIDTH = 600
+LIFT_HEIGHT = 200
+LIFT_X = 0
+LIFT_Y = 0
+
+BASE_WIDTH = 145
+BASE_HEIGHT = 100
+BASE_X = 77
+BASE_Y = WINDOW_HEIGHT-BASE_HEIGHT
+
+RACK_WIDTH = 40
+RACK_HEIGHT = 75
+RACK_CNT = (WINDOW_HEIGHT-BASE_HEIGHT-LIFT_HEIGHT)//RACK_HEIGHT + 1
+RACK_X = 133
+RACK_Y = LIFT_HEIGHT
+
+HOOK_WIDTH = 40
+HOOK_HEIGHT = 50
+HOOK_X_L = 300
+HOOK_X_R = 500
+HOOK_Y = 100
+HOOK_SPEED = BASIC_SPEED
 
 #====颜色
 BLACK           = (  0,   0,   0)
@@ -62,7 +90,7 @@ def main():
     display_surf = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
     pygame.display.set_caption('Ladder')
 
-    showStartScreen()
+    #showStartScreen()
     while True:
         score = runGame()
         showGameOverScreen(score)
@@ -112,10 +140,12 @@ def showGameOverScreen(score):
 def runGame():
     #本地变量，各种标记，缓存
     gameOver = False
+    move = True
     state = STATE["HANG"]
     
     #本地变量，初始化各个元素
     clouds = initialCloud()
+    crane = initialCrane()
     score = initialScore()    
     
     #MUSIC
@@ -141,12 +171,14 @@ def runGame():
         #TODO
 
         #更新各个元素
-        clouds = updateCloud(clouds, state)
-        score = updateScore(score, state)
+        clouds = updateCloud(clouds, move)
+        crane = updateCrane(crane, move, state)
+        score = updateScore(score, move)
         
         #绘图步骤 --------
         drawBackground()
         drawCloud(clouds)
+        drawCrane(crane)
         drawScore(score)
 
         pygame.display.update()
@@ -232,14 +264,20 @@ def initialCloud():
         clouds.append(cloud) 
     return clouds
     
-def updateCloud(clouds, state):
+def updateCloud(clouds, move):
     for i, cloud in enumerate(clouds):
         cloud_img, cloud_pos = cloud[0], cloud[1]
-        cloud_pos[0] += CLOUD_SPEED
+        cloud_pos[0] += np.random.randint(CLOUD_SPEED) #随机移动水平速度
         if cloud_pos[0] > WINDOW_WIDTH:
             cloud_img = pygame.image.load("resource/cloud/cloud"+str(np.random.randint(8))+".png")
             cloud_img = pygame.transform.scale(cloud_img, (CLOUD_WIDTH, CLOUD_HEIGHT))
             cloud_pos = [-CLOUD_WIDTH, np.random.randint(0, high=WINDOW_HEIGHT-CLOUD_HEIGHT)]
+        if move == True:
+            cloud_pos[1] += BASIC_SPEED # 固定移动垂直速度，当视觉整体上移的时候
+            if cloud_pos[1] > WINDOW_HEIGHT:
+                cloud_img = pygame.image.load("resource/cloud/cloud"+str(np.random.randint(8))+".png")
+                cloud_img = pygame.transform.scale(cloud_img, (CLOUD_WIDTH, CLOUD_HEIGHT))
+                cloud_pos = [np.random.randint(0, high=WINDOW_HEIGHT-CLOUD_HEIGHT), -CLOUD_HEIGHT]
         cloud = [cloud_img, cloud_pos]
         clouds[i] = cloud
     return clouds
@@ -251,13 +289,91 @@ def drawCloud(clouds):
         cloud_img, cloud_pos = cloud[0], cloud[1]
         display_surf.blit(cloud_img, (cloud_pos[0], cloud_pos[1]))
     
+def initialCrane():
+    #架子
+    racks = []
+    for i in range(RACK_CNT):
+        rack_y = RACK_Y + RACK_HEIGHT*i
+        racks.append(rack_y)
+    #底座
+    base_y = BASE_Y
+    #钩子
+    hook_x = HOOK_X_L
+    crane = [racks, base_y, hook_x]
+    return crane
+
+hook_move_r = True    
+def updateCrane(crane, move, state):
+    racks, base_y, hook_x = crane[0], crane[1], crane[2]
+    #架子
+    if racks[0] >= RACK_Y and racks[0] < RACK_Y+BASIC_SPEED: #第一节已经要挪出去了，需要在前面新增加一节
+        rack_y_new = RACK_Y - RACK_HEIGHT
+        racks.insert(0, rack_y_new)
+    if racks[-1] >= WINDOW_HEIGHT:
+        racks.pop(-1)
+    if move == True:
+        for i, rack_y in enumerate(racks):
+            rack_y += BASIC_SPEED
+            racks[i] = rack_y
+    #底座
+    if move == True:
+        base_y += BASIC_SPEED
+    #钩子
+    global hook_move_r
+    #TODO: only in free state
+    if hook_move_r == True:
+        if hook_x < HOOK_X_R:
+            hook_x += HOOK_SPEED
+        else:
+            hook_move_r = False
+    else:
+        if hook_x > HOOK_X_L:
+            hook_x -= HOOK_SPEED
+        else:
+            hook_move_r = True
+    
+    crane = [racks, base_y, hook_x]
+    return crane
+    
+def drawCrane(crane):
+    racks, base_y, hook_x = crane[0], crane[1], crane[2]
+    #画架子
+    for rack_y in racks:
+        rack_img = pygame.image.load("resource/crane/rack.png")
+        rack_img = pygame.transform.scale(rack_img, (RACK_WIDTH, RACK_HEIGHT))
+        display_surf.blit(rack_img, (RACK_X, rack_y))
+    #画上部支架
+    lift_img = pygame.image.load("resource/crane/lift.png")      
+    lift_img = pygame.transform.scale(lift_img, (LIFT_WIDTH, LIFT_HEIGHT))      
+    display_surf.blit(lift_img, (LIFT_X, LIFT_Y))
+    #画底座
+    if base_y <= WINDOW_HEIGHT:
+        base_img = pygame.image.load("resource/crane/base.png")      
+        base_img = pygame.transform.scale(base_img, (BASE_WIDTH, BASE_HEIGHT))      
+        display_surf.blit(base_img, (BASE_X, base_y))   
+    #画钩子
+    hook_img = pygame.image.load("resource/crane/hook.png")      
+    hook_img = pygame.transform.scale(hook_img, (HOOK_WIDTH, HOOK_HEIGHT))      
+    display_surf.blit(hook_img, (hook_x, HOOK_Y))
+    
 def initialScore():    
     return 0
 
-def updateScore(score, state):
+def updateScore(score, move):
+    if move == True:
+        score += 1
     return score
     
 def drawScore(score):
+    ruler_img = pygame.image.load("resource/ruler/ruler.png")
+    ruler_img = pygame.transform.scale(ruler_img, (RULER_WIDTH, RULER_HEIGHT))
+    display_surf.blit(ruler_img, (RULER_X, RULER_Y))
+    
+    score_font = pygame.font.Font('freesansbold.ttf', 20)
+    textSurfaceObj = score_font.render(str(score)+"m", True, BLACK, WHITE)
+    textRectObj = textSurfaceObj.get_rect()
+    textRectObj.topleft = (RULER_X, RULER_Y+10)
+    display_surf.blit(textSurfaceObj, textRectObj)
     pass
     
 def drawImageStart():
